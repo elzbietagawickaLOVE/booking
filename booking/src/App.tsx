c3b45c11042c7b60f27a93d1b7d6d6ba25bc38d6
MlodyFati <mateuszsplonskowski@gmail.com>
2024-02-06 21:10:31 +0100

    dodano pierwsze zdięcia i troche podstawowego kodziku do app.tsx i modyfikacja lekka index.html (teraz ide spać)

--- booking/src/App.tsx
+++ booking/src/App.tsx
@@ -1,12 +1,23 @@
 import { useState } from 'react'
-
+// fun fact footerprimitive.png jest po to żeby zobaczyć jakby ta fala wygląda i jak kolor czcionki do niej pasuje ale chyba nie działa (napewno nie działa)
 function App() {
-  const style: React.CSSProperties = { width: 'max-width', height: '100px', background: 'pink' };
   return (
-    <>
-      hej<div style={style}>aaa</div>hej
-    </>
-  )
+    <div>
+      <header style={{ backgroundColor: '#e84686', color: '#550a30', padding: '20px', display: 'flex', alignItems: 'center' }}>
+        <img src='logo.png' height="5%" width="5%" alt="Logo Bootle" style={{marginRight: '10px'}} />
+        <h1>Bootle</h1>
+      </header>
+
+      <main style={{ padding: '20px' }}>
+        <h2>Treść główna</h2>
+        <p>To jest treść główna twojej strony.</p>
+      </main>
+
+      <footer style={{ backgroundColor: '#333', color: '#550a30', padding: '20px', textAlign: 'center',backgroundImage: 'url("footerprimitive.png")', backgroundSize: '50%' }}> 
+        <p>Stopka</p>
+      </footer>
+    </div>
+  );
 }
 
-export default App
+export default App;
